0af40fa234a2f9c76dc1dd2b069b9b16e354c8e9
Navi3321 <ihl3321@g.rit.edu>
2024-03-31 20:23:55 -0400

    adding additional testing methods for null values, different data types, and invalid values

--- src/test/java/EdgeFieldTest.java
+++ src/test/java/EdgeFieldTest.java
@@ -13,94 +13,134 @@ public class EdgeFieldTest {
 
     @Test
     public void testToString() {
-        // Test the toString() method
+        // Checks that the toString() method returns the expected string of EdgeField
         assertEquals("1|Name|0|0|0|0|1|false|false|", edgeField.toString());
     }
 
     @Test
     public void testGetNumFigure() {
-        // Test the getNumFigure() method
+        // Checks that the getNumFigure() method returns the correct value of numFigure
         assertEquals(1, edgeField.getNumFigure());
     }
 
     @Test
     public void testGetName() {
-        // Test the getName() method
+        // Checks that the getName() method returns the correct name value
         assertEquals("Name", edgeField.getName());
     }
 
     @Test
     public void testSetAndGetTableID() {
-        // Test the setTableID() and getTableID() methods
+        // Checks that the setTableID() and getTableID() methods get the same value that is set
         edgeField.setTableID(2);
         assertEquals(2, edgeField.getTableID());
     }
 
     @Test
     public void testSetAndGetTableBound() {
-        // Test the setTableBound() and getTableBound() methods
+        // Checks that the methods have the same set and get value
         edgeField.setTableBound(1);
         assertEquals(1, edgeField.getTableBound());
     }
 
     @Test
     public void testSetAndGetFieldBound() {
-        // Test the setFieldBound() and getFieldBound() methods
+        // Checks that the methods have the same set and get value
         edgeField.setFieldBound(1);
         assertEquals(1, edgeField.getFieldBound());
     }
 
     @Test
     public void testSetAndGetDisallowNull() {
-        // Test the setDisallowNull() and getDisallowNull() methods
+        // Checks that the methods have the same set and get value
         edgeField.setDisallowNull(true);
         assertTrue(edgeField.getDisallowNull());
     }
 
     @Test
     public void testSetAndGetIsPrimaryKey() {
-        // Test the setIsPrimaryKey() and getIsPrimaryKey() methods
+        // Checks that the methods have the same set and get value
         edgeField.setIsPrimaryKey(true);
         assertTrue(edgeField.getIsPrimaryKey());
     }
 
     @Test
-    public void testSetIsPrimaryKeyFalse() {
-        // Test setting primary key without associated table ID
-        edgeField.setTableID(0); // Table ID not set
-        edgeField.setIsPrimaryKey(true); // Trying to set as primary key
-        assertFalse(edgeField.getIsPrimaryKey()); // Should fail, primary key requires table ID
-    }
-
-    @Test
     public void testSetAndGetDefaultValue() {
-        // Test the setDefaultValue() and getDefaultValue() methods
+        // Checks that the methods have the same set and get value
         edgeField.setDefaultValue("Default");
         assertEquals("Default", edgeField.getDefaultValue());
     }
 
     @Test
     public void testSetAndGetVarcharValue() {
-        // Test the setVarcharValue() and getVarcharValue() methods
+        // Checks that the methods have the same set and get value
         edgeField.setVarcharValue(10);
         assertEquals(10, edgeField.getVarcharValue());
     }
 
     @Test
     public void testInvalidSetVarcharValue() {
-        // Test setting an invalid VARCHAR value
-        edgeField.setDataType(0); // Setting data type to VARCHAR
-        edgeField.setVarcharValue(-5); // Negative value is invalid
+        // Checks that the setVarcharValue() method rejects negative values and keeps the default value
+        edgeField.setDataType(0); 
+        edgeField.setVarcharValue(-5); // Should not take in the negative value
         assertEquals(EdgeField.VARCHAR_DEFAULT_LENGTH, edgeField.getVarcharValue());
     }
 
     @Test
     public void testSetAndGetDataType() {
-        // Test the setDataType() and getDataType() methods
+        // Checks that the methods have the same set and get value
         edgeField.setDataType(2);
         assertEquals(2, edgeField.getDataType());
     }
 
+    @Test
+    public void testSetInvalidDataType() {
+        // Checks that the testSetInvalidDataType() method rejects the invalid value and keep the default value
+        edgeField.setDataType(5); // datatype is out of bounds (0 to 3 are valid)
+        assertEquals(0, edgeField.getDataType());
+    }
+
+    @Test
+    public void testSetNegativeFieldBound() {
+        // Checks that the setFieldBound() method does not allow setting a negative field bound and keeps the current value
+        int originalFieldBound = edgeField.getFieldBound();
+        edgeField.setFieldBound(-1); // Should not be set to the negative value
+        assertNotEquals(originalFieldBound, edgeField.getFieldBound());
+    }
+
+    @Test
+    public void testDefaultValueIsNull() {
+        // Checks that there is nothing as the default value(nothing != null)
+        assertEquals("", edgeField.getDefaultValue());
+    }
 
 
+    @Test
+    public void testSetDefaultToNull() {
+        // Chekcs that the setDefaultValue() method can set the default value to null(empty) and not just a string value
+        edgeField.setDefaultValue(null);
+        assertNull(edgeField.getDefaultValue());
+    }
+
+    @Test
+    public void testGetStrDataType() {
+        // Checks that the getStrDataType() method has the correct string data types and nothing more
+        assertArrayEquals(new String[]{"Varchar", "Boolean", "Integer", "Double"}, EdgeField.getStrDataType());
+    }
+
+    @Test
+    public void testDataTypeBoundary() {
+        // Checks that the setDataType() method will not set max datatype value and keep the default
+        edgeField.setDataType(Integer.MAX_VALUE); 
+        assertEquals(0, edgeField.getDataType()); // Data type should stay the same
+    }
+
+    @Test
+    public void testSetAndGetDataTypes() {
+        // Checks that the setDataType() and getDataType() methods can take each data type and set it
+        for (int i = 0; i < EdgeField.getStrDataType().length; i++) {
+            edgeField.setDataType(i);
+            assertEquals(i, edgeField.getDataType());
+        }
+    }
 }
